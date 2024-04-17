f2002fb6e241b8c4e76f6b7f8560b4b5c01de789
cmei <cmei@cmeis-MacBook-Pro.local>
2024-04-17 11:24:30 -0700

    Add web server into the EC2 stance.

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -5,6 +5,13 @@ resource "aws_instance" "example" {
     ami = "ami-051f8a213df8bc089"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello World!" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+    user_data_replace_on_change = true
+
     tags = {
         Name = "Terraform-example"
     }
