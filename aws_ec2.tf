06a902d864fd93be87c255481be7b5aed864844c
cmei <cmei@cmeis-MacBook-Pro.local>
2024-04-17 11:27:14 -0700

    Added Firewall rule to allow ec2 instance can be accessed from internet

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-051f8a213df8bc089"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
             #!/bin/bash
